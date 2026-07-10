77932de3e50e1cea32021ade96fbf326f2580f1c
esmo21 <markussesser@web.de>
2026-07-10 13:42:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/boulder-tracker/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/test/' : '/',
   test: { environment: 'jsdom', globals: true },
 });
